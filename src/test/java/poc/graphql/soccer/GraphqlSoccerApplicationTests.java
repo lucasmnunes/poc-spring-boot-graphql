d970c939ffd67ec29c50d0218629ced288c432f0
Lucas Nunes <lucasnunes000@gmail.com>
2020-08-14 18:54:29 -0300

    Adicionada dependência Spring Boot GraphQL

---
+++ src/test/java/poc/graphql/soccer/GraphqlSoccerApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.graphql.soccer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSoccerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
