0ba48310ef1c3f7f8b15c5fee7837723bffcffa3
Lucas Nunes <lucasnunes000@gmail.com>
2020-08-16 13:58:23 -0300

    Implementação do modelo 'Player'

---
+++ src/main/java/poc/graphql/soccer/model/Player.java
@@ -0,0 +1,39 @@
+package poc.graphql.soccer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Player")
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public int id;
+
+    @Column
+    public String name;
+
+    @Column
+    public String country;
+
+    @Column
+    public String position;
+
+    @Column
+    public int jerseyNumber;
+
+}
