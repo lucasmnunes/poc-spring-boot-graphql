c566035dbf9bb75ec192cae8e381e18ef566461f
Lucas Nunes <lucasnunes000@gmail.com>
2020-08-17 12:27:57 -0300

    Adicionado script de inserção de Players

---
+++ src/main/resources/data.sql
@@ -0,0 +1,32 @@
+INSERT INTO player (id, name, country, position, jersey_number)
+    VALUES (1, 'Iker Casillas', 'Spain', 'Goalkeeper', 1);
+
+INSERT INTO player (id, name, country, position, jersey_number)
+    VALUES (2, 'Michel Salgado', 'Spain', 'Defender', 2);
+
+INSERT INTO player (id, name, country, position, jersey_number)
+    VALUES (3, 'Fernando Hierro Carlos', 'Spain', 'Defender', 4);
+
+INSERT INTO player (id, name, country, position, jersey_number)
+    VALUES (4, 'Ivan Helguera', 'Spain', 'Defender', 6);
+
+INSERT INTO player (id, name, country, position, jersey_number)
+    VALUES (5, 'Roberto Carlos', 'Brazil', 'Defender', 3);
+
+INSERT INTO player (id, name, country, position, jersey_number)
+    VALUES (6, 'Guti', 'Spain', 'Midfielder', 14);
+
+INSERT INTO player (id, name, country, position, jersey_number)
+    VALUES (7, 'Zinedine Zidane', 'France', 'Midfielder', 5);
+
+INSERT INTO player (id, name, country, position, jersey_number)
+    VALUES (8, 'David Beckham', 'England', 'Midfielder', 23);
+
+INSERT INTO player (id, name, country, position, jersey_number)
+    VALUES (9, 'Luis Figo', 'Portugal', 'Midfielder', 10);
+
+INSERT INTO player (id, name, country, position, jersey_number)
+    VALUES (10, 'Raul', 'Spain', 'Attacker', 7);
+
+INSERT INTO player (id, name, country, position, jersey_number)
+    VALUES (11, 'Ronaldo', 'Brazil', 'Attacker', 9);
